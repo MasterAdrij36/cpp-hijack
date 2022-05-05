099ba2d0c4bdee9b35a7f3dc365bc2ed56b75866
Adrij Ghosh <adrijghosh@Adrijs-MacBook-Air.local>
2022-05-05 20:00:32 +0530

    Sum till n3 using recursion.

---
+++ 06Recursion/sum_N3_Rec.cpp
@@ -0,0 +1,16 @@
+// 1 -> 1 ; 2 -> 9 ; 3 -> 36 
+
+#include<iostream>
+using namespace std;
+
+int SumRecN3(int n) {
+    if (n==1) return 1;
+    return n*n*n+SumRecN3(n-1);
+}
+
+int main() {
+    int n;
+    cin>>n;
+    cout<<SumRecN3(n)<<"\n";
+    return 0;
+}
