25435ff92c0e8e0b636611d7233cdd02dff6d836
Adrij Ghosh <adrijghosh@Adrijs-MacBook-Air.local>
2022-06-07 20:42:21 +0530

    Inplace Transpose of Matrix (modified).

--- 07Matrix/matrixTranspose.cpp
+++ 07Matrix/matrixTranspose.cpp
@@ -2,6 +2,16 @@
 #include<vector>
 using namespace std;
 
+void Print (const vector<vector <int> > &arr) {
+    int n = arr.size();
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<n; j++) {
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+}
+
 vector<vector<int> > inPlaceTranspose(vector<vector<int> > arr) {
     int n=arr.size();
     for (int i=0; i<n; i++) {
@@ -15,6 +25,7 @@ vector<vector<int> > inPlaceTranspose(vector<vector<int> > arr) {
 int main() {
     int n;
     cin>>n;
+    cout<<"\n";
     vector<vector<int> > arr(n, vector<int> (n));
     int k=1;
     for (int i=0; i<n; i++) {
@@ -22,13 +33,10 @@ int main() {
             arr[i][j]=k++;
         }
     }
+    Print(arr);
+    cout<<"\n";
     arr = inPlaceTranspose(arr);
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<n; j++) {
-            cout<<arr[i][j]<<" ";
-        }
-        cout<<"\n";
-    }
+    Print(arr);
     cout<<"\n";
     return 0;
 }
